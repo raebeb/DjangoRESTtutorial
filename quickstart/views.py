74bf717eb0d6885607ebee950e5ce2f58bd6b7f6
Francisca <raebeb@gmail.com>
2020-10-18 21:00:08 -0300

    views and urls fixed

--- quickstart/views.py
+++ quickstart/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from quickstart.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
